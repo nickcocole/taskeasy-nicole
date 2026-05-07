a49fb0925aedfb566861965aeddcaa86923ca61d
liviaacristaldo <liviaacristaldo@gmail.com>
2026-05-07 11:37:40 -0400

    Adicionado menu inicial

--- src/taskeasy/TaskEasy.java
+++ src/taskeasy/TaskEasy.java
@@ -5,6 +5,8 @@ public class TaskEasy {
 
     public static void main(String[] args) {
         System.out.println("Bem-vindo ao sistema TaskEasy");
+        System.out.println("1 - Cadastrar tarefa");
+        System.out.println("2 - Listar tarefas");
     }
     
 }
